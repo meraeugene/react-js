205edd77beba5c3203666babc8209bc09a2bf74d
Andrew R. Villalon <villalon.andrew123@gmail.com>
2024-05-01 20:52:16 +0800

    react-installation

---
+++ my-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <h1 className="text-7xl font-bold flex items-center justify-center h-screen  ">
+      Hello world!
+    </h1>
+  );
+};
+
+export default App;
